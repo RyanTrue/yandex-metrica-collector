8c6fe76cd0a491d1997ccaf42e147f380f7dcece
ryan <zufirka@icloud.com>
2023-11-09 20:16:12 +0300

    iter19

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,43 +2,100 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
-	agent2 "github.com/RyanTrue/yandex-metrica-collector/internal/agent"
 	"github.com/RyanTrue/yandex-metrica-collector/internal/collector"
 	"github.com/RyanTrue/yandex-metrica-collector/internal/flags"
 	log "github.com/RyanTrue/yandex-metrica-collector/internal/logger"
-	aggregator "github.com/RyanTrue/yandex-metrica-collector/internal/metrics"
+	"github.com/RyanTrue/yandex-metrica-collector/internal/router/router"
+	"github.com/RyanTrue/yandex-metrica-collector/internal/saver/database"
+	"github.com/RyanTrue/yandex-metrica-collector/internal/saver/file"
 	"go.uber.org/zap"
-	"golang.org/x/sync/errgroup"
-	"os"
+	"net/http"
+	_ "net/http/pprof"
 )
 
+const pprofAddr string = ":90"
+
 func main() {
-	params := flags.Init(
-		flags.WithPollInterval(),
-		flags.WithReportInterval(),
-		flags.WithAddr(),
-		flags.WithKey(),
-		flags.WithRateLimit(),
-	)
-
-	errs, ctx := errgroup.WithContext(context.Background())
-
 	logger, err := zap.NewDevelopment()
 	if err != nil {
-		os.Exit(1)
+		fmt.Println("error while creating logger, exit")
+		return
 	}
 	defer logger.Sync()
+
 	log.SugarLogger = *logger.Sugar()
 
-	agent := agent2.New(params, aggregator.New(&collector.Collector), log.SugarLogger)
-	errs.Go(func() error {
-		return agent.CollectMetrics(ctx)
-	})
-	errs.Go(func() error {
-		return agent.SendMetrics(ctx)
-	})
-	if err := errs.Wait(); err != nil {
-		log.SugarLogger.Errorf(fmt.Sprintf("error while running agent: %s", err.Error()))
+	params := flags.Init(
+		flags.WithAddr(),
+		flags.WithStoreInterval(),
+		flags.WithFileStoragePath(),
+		flags.WithRestore(),
+		flags.WithDatabase(),
+		flags.WithKey(),
+	)
+
+	r := router.New(*params)
+
+	log.SugarLogger.Infow(
+		"Starting server",
+		"addr", params.FlagRunAddr)
+
+	// init restorer
+	var saver saver
+	if params.FileStoragePath != "" && params.DatabaseAddress == "" {
+		saver = file.New(params.FileStoragePath)
+	} else if params.DatabaseAddress != "" {
+		db, err := sql.Open("pgx", params.DatabaseAddress)
+		if err != nil {
+			log.SugarLogger.Error(err.Error(), "open db error")
+			return
+		}
+		saver, err = database.New(db)
+		if err != nil {
+			log.SugarLogger.Errorf(err.Error())
+		}
 	}
+
+	// restore previous metrics if needed
+	ctx := context.Background()
+	//TODO: хотелось бы избавиться от этого if и от того, что на 62 строке, как можно сделать лучше?
+	if params.Restore && (params.FileStoragePath != "" || params.DatabaseAddress != "") {
+		metrics, err := saver.Restore(ctx)
+		if err != nil {
+			log.SugarLogger.Error(err.Error(), "restore error")
+		}
+		collector.Collector.Metrics = metrics
+		log.SugarLogger.Info("metrics restored")
+	}
+
+	// regularly save metrics if needed
+	if params.DatabaseAddress != "" || params.FileStoragePath != "" {
+		go saveMetrics(ctx, saver, params.StoreInterval)
+	}
+
+	//if err := http.ListenAndServe(pprofAddr, nil); err != nil {
+	//	log.SugarLogger.Fatalw(err.Error(), "pprof", "start pprof server")
+	//}
+	// run server
+	if err := http.ListenAndServe(params.FlagRunAddr, r); err != nil {
+		log.SugarLogger.Fatalw(err.Error(), "event", "start server")
+	}
+}
+
+func saveMetrics(ctx context.Context, saver saver, interval int) {
+	//здесь должен быть нормальный тикер, но тесты метрики не ждут
+	//ticker := time.NewTicker(time.Duration(interval))
+	for {
+		if err := saver.Save(ctx, collector.Collector.Metrics); err != nil {
+			log.SugarLogger.Error(err.Error(), "save error")
+		}
+		//time.Sleep(time.Duration(interval))
+	}
+}
+
+type saver interface {
+	Restore(ctx context.Context) ([]collector.StoredMetric, error)
+	Save(ctx context.Context, metrics []collector.StoredMetric) error
 }
--- go.mod
+++ go.mod
@@ -11,7 +11,9 @@ require (
 	github.com/shirou/gopsutil/v3 v3.23.4
 	github.com/stretchr/testify v1.8.2
 	go.uber.org/zap v1.24.0
-	golang.org/x/sync v0.1.0
+	golang.org/x/sync v0.3.0
+	golang.org/x/tools v0.12.1-0.20230825192346-2191a27a6dc5
+	honnef.co/go/tools v0.4.6
 )
 
 require (
@@ -30,9 +32,11 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.6.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
